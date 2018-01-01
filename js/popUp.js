ff1a276a145ad10d41aa4b7bfb250ffcbc223415
Евгения Масликова < >
2018-01-01 19:34:57 +0300

    goods in order

--- js/popUp.js
+++ js/popUp.js
@@ -1,7 +1,20 @@
 
-   $(document).ready(function(){
-         
-        PopUpHide();
+   $(document).ready(function(){         
+    PopUpHide();
+    $('.buy_button').click(function(e){
+    var buttonClicked = e.target;     
+    var buttonPressed = e.target.parentElement; 
+     console.log(buttonPressed);     
+    var product= $(buttonPressed).children(".model").val();
+    console.log(product);
+    
+    $("#popup2").show();
+    $("#blindLayer").css("display","block"); 
+    $("#deal").text("Заказать " + product);
+    // $('#buy_product').append("<p>" + product + "</p>");
+    $("#product_to_buy").val(product);
+           
+    });
     });
 
     //Функция отображения PopUp
@@ -16,10 +29,26 @@
         $("#blindLayer").css("display","none");
     }
 
-function PopUpTwoHide() {
+function PopUpShow2() {
+    $('#popup2').show();
+    $("#blindLayer").css("display","block");
+}
+
+function PopUpHide2() {
     $('#popup2').hide();
     $("#blindLayer").css("display","none");
 }
+
+// $(document).ready(function(){
+//     $('.buy_product').click(function(e){
+//       var buttonClicked = e.target;
+     
+//       var buttonPressed = e.target.parentElement;
+      
+//       var product= $(buttonPressed).children(".model").text();
+        
+//         });
+//             });
 
 
 
